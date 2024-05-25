4a0bb82b5e66cc9a375b50081bb24750b887072d
nttduongg <21130032@st.hcmuaf.edu.vn>
2024-05-25 09:39:52 +0700

    them textview vao layout va tao them textview trong activy

--- app/src/main/java/com/example/mydemogit/ActivityLayout.java
+++ app/src/main/java/com/example/mydemogit/ActivityLayout.java
@@ -1,6 +1,7 @@
 package com.example.mydemogit;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,8 @@ public class ActivityLayout extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+
+        //goi them edit text vao
+        EditText edit = findViewById(R.id.edit);
     }
 }
